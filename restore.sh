3ceca8b63c23cce67636263a8d40c595e99ccd45
nobody48sheldor <lolmyizi@gmail.com>
2025-10-19 14:55:28 +0200

    moves node to focused desktop before hidden=on

--- restore.sh
+++ restore.sh
@@ -14,7 +14,8 @@ mon_h=$(echo "$monitor" | jq '.rectangle.height')
 
 # Float and center
 bspc node "$wid" -t floating;
-bspc node "$wid" -g hidden=off
+bspc node "$wid" -d focused;
+bspc node "$wid" -g hidden=off;
 
 pos_x=$(($mon_w/2 - 100))
 pos_y=$(($mon_h/2 - 500))
